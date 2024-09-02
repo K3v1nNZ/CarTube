bd4e40bc7dcf8401cb1d2671d22dc91f4b7cb8ef
K3v1nNZ <icegamer37@gmail.com>
2024-09-03 11:52:59 +1200

    yes

--- CarTube/Util/Constants.swift
+++ CarTube/Util/Constants.swift
@@ -7,6 +7,6 @@
 
 import Foundation
 
-let YT_HOME = "https://m.youtube.com/"
+let YT_HOME = "https://discord.com/app"
 let YT_EMBED = "https://m.youtube.com/watch?v="
 let YT_SEARCH = "https://m.youtube.com/results?search_query="
